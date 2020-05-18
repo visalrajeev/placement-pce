e8586caffee7c15d879cccd9e9f7e3757540bead
visalrajeev <49332745+visalrajeev@users.noreply.github.com>
2020-05-18 15:56:23 +0530

    Add files via upload

---
+++ student/footer.php
@@ -0,0 +1,57 @@
+<div class="footer">
+  <div class="container">
+    <div class="col-md-3 ftr_navi ftr">
+      <h3>NAVIGATION</h3>
+      <ul>
+        <li>
+          <a href="../index.php">Home</a>
+        </li>
+        <li>
+          <a href="./login.php">Student Login</a>
+        </li>
+        <li>
+          <a href="../companies/login.php">Company Login</a>
+        </li>
+
+        <li>
+          <a href="../admin/login.php">Admin Login</a>
+        </li>
+      </ul>
+    </div>
+    <!-- <div class="col-md-3 ftr_navi ftr">
+      <h3>MEMBERS</h3>
+      <ul>
+        <li>
+          <a href="#">Customer Support</a>
+        </li>
+        <li>
+          <a href="#">Placement Support</a>
+        </li>
+        <li>
+          <a href="#">Faculty Support</a>
+        </li>
+        <li>
+          <a href="#">Registered Companies</a>
+        </li>
+        <li>
+          <a href="#">Training</a>
+        </li>
+      </ul>
+    </div> -->
+    <div class="col-md-3 get_in_touch ftr">
+      <h3>CONTACT</h3>
+      <p>Dr. K. M. Vasudevan Pillai Campus</p>
+      <p>Plot No. 10, Sector 16, New Panvel – 410 206</p>
+      <p>Landline: 022 – 2745 6030 / 1700 / 6100</p>
+      <a href="mailto:djsce@gmail.com">pce@mes.ac.in</a>
+    </div>
+    <div class="col-md-3 get_in_touch ftr">
+
+    </div>
+    <div class="col-md-3 ftr-logo">
+      <p>Copyright &copy; MES-2020
+
+    </div>
+    </div>
+
+  </div>
